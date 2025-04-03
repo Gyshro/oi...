24d3a30cf4b8f386481e6b9d2c9566d99aa756ec
Maxwell Gomes <maxgommax@gmail.com>
2025-04-03 17:02:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+    let n = prompt("Oi, Cerejinha... você tá cheirosa, sabia?");
+    
+   
+    if (n.length > 0) {
+        alert("Aiai... Você já parou pra perceber como o mundo fica mais bonito quando você está nele?");
+        alert("Não é só o seu jeito de falar, ou de olhar... é a forma como você existe, e sem nem perceber... você é assim sem querer, contagiante e por isso te plagiam.");
+        alert("Você acha que está apenas vivendo o seu dia a dia, mas pra quem te vê de fora, você é a cena mais bonita do filme que ninguém quer que acabe" + n);
+        alert("E como eu sempre te digo... se algum dia duvidar do quanto você é grandiosa, olha ao seu redor... você é admirada, querendo ou não");
+        alert("E se o mundo fosse uma tela em branco, você seria a cor mais vibrante");
+        alert("Cada coisa que você toca carrega um pedaço de você e da tua beleza exímia");
+        alert("Até o que você acha simples tem uma beleza que só existe porque veio de você, ESPECIFICAMENTE de você, porque tudo o que faz é extraordinário");
+        alert("e tenho que te dizer mais... algumas ideias surgem como fagulhas passageiras, mas as suas não... as suas têm a imponência de catedrais, o requinte de salões iluminados por candelabros dourados, a intemporalidade de algo que jamais poderia ser esquecido");
+        alert("seus gostos não são simples preferências, são como relíquias de uma outra era, escolhidas a dedo, refinadas pelo tempo... como manuscritos valiosos, como um baile exclusivo onde poucos têm o privilégio de entender a beleza do que você aprecia");
+        alert("e então vem sua espontaneidade, o golpe final... quando menos se espera, você surge com um comentário certeiro, uma ideia imprevisível, uma reviravolta que faz tudo ficar mais interessante... você é um livro que nunca se repete, uma história que ninguém se cansa de ler");
+        alert("e saiba que sem você, o mundo seria um rascunho inacabado.");
+                
+        break;
+    }
+}
